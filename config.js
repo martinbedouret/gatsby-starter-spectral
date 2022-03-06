3ecd0ba8fcec7a24a75a2186469a64917b3d2271
martin bedouret <martinbedouret@gmail.com>
2022-03-05 21:31:44 -0300

    wip

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'WE	DESIGN	UNIQUE PRODUCTS FOR STARTUPS, COMPANIES AND	EVERYTHING IN BETWEEN',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  heading: 'We design unique products for startups, companies, and everything in between',
+  subHeading: 'We are an international multidisciplinary design studio based in Buenos Aires and Stockholm',
   // social
   socialLinks: [
     {

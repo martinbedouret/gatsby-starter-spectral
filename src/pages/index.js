dd8ac42cfd4744b62ea68a437e78f158a6e597eb
martin bedouret <martinbedouret@gmail.com>
2022-03-05 23:06:25 -0300

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -156,16 +156,12 @@ const IndexPage = () => (
     <section id="cta" className="wrapper style4">
       <div className="inner">
         <header>
-          <h2>Arcue ut vel commodo</h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum endrerit imperdiet amet
-            eleifend fringilla.
-          </p>
+          <h2>We	bring LIFE to	your IDEAS</h2>
         </header>
         <ul className="actions stacked">
           <li>
             <a href="/#" className="button fit primary">
-              Activate
+              Contact Us
             </a>
           </li>
           <li>
